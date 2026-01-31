b1816cf35f2763f3f3da71c3dc4bf984ac965799
Nitish Kumar <nk2332001@gmail.com>
2026-01-31 05:35:23 +0530

    implemented real time drawing sync across all users using socket.io

--- client/src/components/WhiteBoard.jsx
+++ client/src/components/WhiteBoard.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 
+import { useSocket } from "../context/SocketContext";
+
 const WhiteBoard = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-  const[isDrawing , setIsDrawing] = useState(false);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const socket = useSocket();
 
   useEffect(() => {
     const canvas = canvasRef.current;
+
+    
 
     // Make it high resolution (HiDPI screens)
     canvas.width = window.innerWidth * 2;
@@ -23,6 +28,31 @@ const WhiteBoard = () => {
     contextRef.current = ctx;
   }, []);
 
+  //using this useEffect to handle socket
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleBeginPath = ({ x, y }) => {
+      const ctx = contextRef.current;
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+    };
+
+    const handleDrawLine = ({ x, y }) => {
+      const ctx = contextRef.current;
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    };
+
+    socket.on("begin_path", handleBeginPath);
+    socket.on("draw_line", handleDrawLine);
+
+    return () => {
+      socket.off("begin_path", handleBeginPath);
+      socket.off("draw_line", handleDrawLine);
+    };
+  }, [socket]);
+
   const startDrawing = (e) => {
     //we just need native event to work with
     const nativeEvent = e.nativeEvent;
@@ -33,6 +63,9 @@ const WhiteBoard = () => {
     contextRef.current.beginPath();
     contextRef.current.moveTo(offsetX, offsetY);
     setIsDrawing(true);
+
+    //this is to tell server what current user doing so that it can broadcast
+    socket.emit("begin_path", { x: offsetX, y: offsetY });
   };
 
   const finishDrawing = () => {
@@ -47,6 +80,9 @@ const WhiteBoard = () => {
     const { offsetX, offsetY } = nativeEvent;
     contextRef.current.lineTo(offsetX, offsetY);
     contextRef.current.stroke();
+
+    //this is to tell server that the current user started drawing
+    socket.emit("draw_line", { x: offsetX, y: offsetY });
   };
 
   return (
@@ -59,6 +95,5 @@ const WhiteBoard = () => {
     />
   );
 };
-
 
 export default WhiteBoard;
--- server/sockets/SocketHandler.js
+++ server/sockets/SocketHandler.js
@@ -1,9 +1,19 @@
 export const setupSocketEvents = (io) => {
   io.on("connection", (socket) => {
-    console.log(`User Connected , User id: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("begin_path", (arg) => {
+      socket.broadcast.emit("begin_path", arg);
+    });
+
+    socket.on("draw_line", (arg) => {
+      socket.broadcast.emit("draw_line", arg);
+    });
 
     socket.on("disconnect", () => {
-      console.log(`User Disconnected , User id:${socket.id}`);
+      console.log(`User Diconnected ${socket.id}`);
     });
   });
+
+
 };
